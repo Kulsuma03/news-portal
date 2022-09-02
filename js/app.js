efa5afd9d7fea91ee5d0cd5070629675de62a324
Kulsuma Akter <kulsumams72@gmail.com>
2022-09-03 01:04:33 +0600

    category setting

--- js/app.js
+++ js/app.js
@@ -2,6 +2,26 @@ const allCategory = async () =>{
     const url = ` https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data.news_category);
+    displayCategory(data.data.news_category);
 }
+
+const displayCategory = allCategory =>{
+    console.log(allCategory);
+    const categoryContainer = document.getElementById('category');
+    
+    allCategory.forEach(category => {
+        console.log('ccc',category)
+        const div = document.createElement('div')
+        div.classList.add('list-group','d-flex')
+        const {category_id, category_name} = category;
+        div.innerHTML = `
+        <p>${category_name}</p>
+        ` 
+        categoryContainer.appendChild(div); 
+    });
+    
+    
+}
+
+
 allCategory()
